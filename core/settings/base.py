56975efddd28f07c347c41e52a3d67e3032f0f5d
Mishary Aljarie <mesh5874@gmail.com>
2022-03-21 16:33:21 +0300

    Add AWS S3 configurations to the production settings.

--- core/settings/base.py
+++ core/settings/base.py
@@ -105,24 +105,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = 'static/'
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# Base url to serve media files
-MEDIA_URL = '/media/'
-
-# Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,4 +1,6 @@
 from .base import *
+
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7-u^#qq%wolcakd+^=of4d0-h3p@e6m3j&43fl6ce&ngrt21h7'
 
@@ -6,3 +8,16 @@ SECRET_KEY = 'django-insecure-7-u^#qq%wolcakd+^=of4d0-h3p@e6m3j&43fl6ce&ngrt21h7
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
+
+# Base url to serve media files
+MEDIA_URL = 'media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
